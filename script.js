f29a9ea45d2b8d0c000b26d609d0d75c5e5120c9
gareglad <gareglad@gmail.co>
2024-05-22 17:00:32 -0400

    11

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
-    const audio = new Audio("https://stream.mubert.com/b2b/v2?playlist=6.4.2&pat=bXViZXJ0Zm9yc3RyZWFtZXJzLjIyMzcyMDYxLjAzZTgxNTg5NzJmMWMzM2IxM2Y2ZDVlOWQ2ZWI3MTdkYTNkOTM3NTcuMS4z.afa86d1422e4128b8f149c9e3927db5e5b6d0f408caecc3b793fe30d7f09b160");
+    //const audio = new Audio("https://stream.mubert.com/b2b/v2?playlist=6.4.2&pat=bXViZXJ0Zm9yc3RyZWFtZXJzLjIyMzcyMDYxLjAzZTgxNTg5NzJmMWMzM2IxM2Y2ZDVlOWQ2ZWI3MTdkYTNkOTM3NTcuMS4z.afa86d1422e4128b8f149c9e3927db5e5b6d0f408caecc3b793fe30d7f09b160");
+    const audio = document.getElementById("audio");
+    
     let playing = false;
     $('#play').click(function() {
         if(playing){
