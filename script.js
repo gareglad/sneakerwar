a59871609d06716e3afcc1d0a96b6f4a9787359d
gareglad <66722619+gareglad@users.noreply.github.com>
2024-05-25 22:33:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     var audioElement = document.createElement('audio');
 
-    var ran1 = Math.floor(Math.random() * 1);
+    var ran1 = Math.floor(Math.random() * 6);
     audioElement.currentTime = ran1;
 
     audioElement.setAttribute('src', 'Trap'+ran1+'.mp3');
